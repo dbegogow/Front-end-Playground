9d026aa5cc7765541176fbad189ebb08453572a9
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-02-17 18:06:03 +0200

    User @ViewChild with ElementRef

--- Angular/DemoProject/hotelinventoryapp/src/app/app.component.ts
+++ Angular/DemoProject/hotelinventoryapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
@@ -6,13 +6,19 @@ import { RoomsComponent } from './rooms/rooms.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit {
   title = 'hotelinventoryapp';
 
-  @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
+  @ViewChild('name', { static: true }) name!: ElementRef;
 
-  ngAfterViewInit() {
-    const componentRef = this.vcr.createComponent(RoomsComponent);
-    componentRef.instance.numberOfRooms = 50;
+  ngOnInit() {
+    this.name.nativeElement.innerText = 'Hilton Hotel';
   }
+
+  // @ViewChild('user', { read: ViewContainerRef }) vcr!:   ViewContainerRef;
+
+  // ngAfterViewInit() {
+  //   const componentRef = this.vcr.createComponent(RoomsComponent);
+  //   componentRef.instance.numberOfRooms = 50;
+  // }
 }
