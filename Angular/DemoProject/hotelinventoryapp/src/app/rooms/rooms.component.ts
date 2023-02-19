e20d79d0c0260857fd10e63f82a2a57cb38d001d
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-02-19 11:00:55 +0200

    HttpService , RxJs observables and http get

--- Angular/DemoProject/hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ Angular/DemoProject/hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -37,7 +37,9 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit, AfterView
   constructor(@SkipSelf() private roomsService: RoomsService) { }
 
   ngOnInit(): void {
-    this.roomList = this.roomsService.getRooms();
+    this.roomsService.getRooms().subscribe(rooms => {
+      this.roomList = rooms;
+    });
   }
 
   ngDoCheck(): void {
@@ -63,7 +65,7 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit, AfterView
 
   addRoom() {
     const room: RoomList = {
-      roomNumber: 4,
+      roomNumber: '4',
       roomType: 'Deluxe Room',
       amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
       price: 500,
--- Angular/DemoProject/hotelinventoryapp/src/app/rooms/services/rooms.service.ts
+++ Angular/DemoProject/hotelinventoryapp/src/app/rooms/services/rooms.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { AppConfig } from '../../AppConfig/appconfig.interface';
 import { APP_SERVICE_CONFIG } from '../../AppConfig/appconfig.service';
@@ -8,44 +9,16 @@ import { RoomList } from '../rooms';
 })
 export class RoomsService {
   roomList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 500,
-      photos: 'https://www.theghotel.ie/wp-content/uploads/2020/09/Deluxe-Room.jpg',
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-      rating: 4.5
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 1000,
-      photos: 'https://www.theghotel.ie/wp-content/uploads/2020/09/Deluxe-Room.jpg',
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-      rating: 3.45654
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 15000,
-      photos: 'https://www.theghotel.ie/wp-content/uploads/2020/09/Deluxe-Room.jpg',
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-      rating: 2.6
-    }
   ];
 
-  constructor(@Inject(APP_SERVICE_CONFIG) private config: AppConfig) {
+  constructor(
+    @Inject(APP_SERVICE_CONFIG) private config: AppConfig,
+    private http: HttpClient) {
     console.log(this.config.apiEndpoint);
     console.log('Rooms Service Initialize');
   }
 
   getRooms() {
-    return this.roomList;
+    return this.http.get<RoomList[]>('/api/rooms');
   }
 }
