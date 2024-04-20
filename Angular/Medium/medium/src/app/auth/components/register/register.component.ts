d27ac5af82ce7ee057bba04a32e5a1ab7ab71f14
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2024-04-20 17:43:10 +0300

    Implement layout

--- Angular/Medium/medium/src/app/auth/components/register/register.component.ts
+++ Angular/Medium/medium/src/app/auth/components/register/register.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
+  imports: [ReactiveFormsModule],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue);
+  }
+}
