af5096bcd10bbceada9de61f52eaf9031af64c3c
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2024-06-01 12:25:41 +0300

    Adding with reducer

--- Angular/Medium/medium/src/main.ts
+++ Angular/Medium/medium/src/main.ts
@@ -1,15 +1,17 @@
 import { isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { appRoutes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { authFeatureKey, authReducer } from './app/auth/store/reducers';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes),
     provideStore(),
+    provideState(authFeatureKey, authReducer),
     provideStoreDevtools({
       maxAge: 25,
       logOnly: !isDevMode(),
