630e6879eb4317e114aec9955405520c59c92f04
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2025-02-08 23:35:50 +0200

    Add example with async pipe

--- Angular/performance-optimization-techniques/src/app/app.component.ts
+++ Angular/performance-optimization-techniques/src/app/app.component.ts
@@ -1,22 +1,31 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
 import { ChannelComponent } from './channel/channel.component';
+import { AsyncPipe } from '@angular/common';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template: `
-   <h2>{{ topicName }}</h2>
-   <button (click)="changeTopic()">Change Topic</button>
-   <button (click)="changeChannel()">Change Channel</button>
-   <app-channel [name]="name"></app-channel>
-    `,
+    <h2>{{ topicName }}</h2>
+    <button (click)="changeTopic()">Change Topic</button>
+    <button (click)="changeChannel()">Change Channel</button>
+    <app-channel [name]="name"></app-channel>
+    <h2>{{ counter$ | async }}</h2>
+  `,
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [ChannelComponent],
+  imports: [ChannelComponent, AsyncPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  name = 'OnPush strategy'
+  name = 'OnPush strategy';
   topicName = 'Change Detection in Angular';
+
+  counter$ = interval(1000);
 
   // constructor(private cdr: ChangeDetectorRef) {
   //   setTimeout(() => {
@@ -31,6 +40,6 @@ export class AppComponent {
   }
 
   changeChannel() {
-    this.name = 'Angular'
+    this.name = 'Angular';
   }
 }
--- Angular/performance-optimization-techniques/src/app/channel/channel.component.ts
+++ Angular/performance-optimization-techniques/src/app/channel/channel.component.ts
@@ -1,12 +1,12 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-channel',
-    template: `<p>Channel "{{name}}"</p>`,
-    styleUrls: ['./channel.component.scss'],
-    standalone: true,
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-channel',
+  template: `<p>Channel "{{ name }}"</p>`,
+  styleUrls: ['./channel.component.scss'],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChannelComponent {
-    @Input() name = '';
+  @Input() name = '';
 }
