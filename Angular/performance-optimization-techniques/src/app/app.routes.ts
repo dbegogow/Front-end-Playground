ff5a336c11c6b7978acce6bc964c4c164cbc6b56
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2025-02-22 23:56:53 +0200

    Register content path

--- Angular/performance-optimization-techniques/src/app/app.routes.ts
+++ Angular/performance-optimization-techniques/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContentComponent } from './content/content.component';
 
 export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
     title: 'Dashboard',
+  },
+  {
+    path: 'content',
+    component: ContentComponent,
+    title: 'Content',
   },
 ];
