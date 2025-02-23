4e459ca9c7c8fd240bdfd35d6f531602aff905ed
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2025-02-23 10:36:31 +0200

    Create youtube video component

--- Angular/performance-optimization-techniques/src/app/content/posts/posts.component.ts
+++ Angular/performance-optimization-techniques/src/app/content/posts/posts.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import YoutubeVideoComponent from './youtube-video/youtube-video.component';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
   standalone: true,
+  imports: [YoutubeVideoComponent],
 })
 export default class PostsComponent {}
---
+++ Angular/performance-optimization-techniques/src/app/content/posts/youtube-video/youtube-video.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-youtube-video',
+  templateUrl: './youtube-video.component.html',
+  styleUrls: ['./youtube-video.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
+})
+export default class YoutubeVideoComponent {
+  @Input() videoId!: string;
+  isPlaying = false;
+  safeVideoUrl!: SafeResourceUrl; // Store sanitized URL
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  get thumbnailUrl(): string {
+    return `https://img.youtube.com/vi/${this.videoId}/hqdefault.jpg`;
+  }
+
+  private get videoUrl(): string {
+    return `https://www.youtube.com/embed/${this.videoId}?autoplay=1&rel=0`;
+  }
+
+  playVideo() {
+    this.safeVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoUrl);
+    this.isPlaying = true;
+  }
+}
